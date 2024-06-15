cb5b61a56faf76076b93488910f9ed9c0c94d0de
nurullailia20 <nurullailia720@gmail.com>
2024-06-15 09:39:25 +0700

    layouting sidebar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,32 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import SideBar from "./SideBar";
 import ProfileCard from "./ProfileCard";
-import { useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
-  // const [isLogin, setIsLogin] = useState(false);
+  // export default function Layout({ children }) {
+//   // const [isLogin, setIsLogin] = useState(false);
 
-  // // Fungsi untuk menangani login yang bisa kamu pasing dari komponen anak ke Layout
-  // const handleLogin = () => {
-  //   setIsLogin(true);
-  // };
+//   // // Fungsi untuk menangani login yang bisa kamu pasing dari komponen anak ke Layout
+//   // const handleLogin = () => {
+//   //   setIsLogin(true);
+//   // };
   const { pathname } = useRouter();
-
   return (
     <>
-      {pathname == "/product" || "/member" ? (
-        <div className="w-full h-screen flex flex-col">
-          <Header />
-          <Navbar />
-
-          <main className="flex-1 bg-[url('/assets/background.jpg')] bg-cover bg-opacity-75">
-            {children}
-          </main>
-        </div>
-      ) : (
+      {pathname === "/product" || pathname === "/member" ? (
         <div className="w-full h-screen p-5 flex gap-5 bg-[url('/assets/background.jpg')] bg-cover bg-opacity-75">
           <SideBar />
 
@@ -35,9 +25,16 @@ export default function Layout({ children }) {
             <section className="flex-1">{children}</section>
           </main>
         </div>
+      ): (
+        <div className="w-full h-screen flex flex-col">
+          <Header />
+          <Navbar />
+
+          <main className="flex-1 bg-[url('/assets/background.jpg')] bg-cover bg-opacity-75">
+            {children}
+          </main>
+        </div>
       )}
     </>
   );
 }
-
-
